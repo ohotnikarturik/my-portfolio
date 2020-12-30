f4eaf7e8ae7c06f718ea6f2b0cdb3b7a6e1b5c78
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2020-12-31 01:45:54 +0200

    take away strict mode, fillerStyle bg important

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import state from "./state";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App state={state} />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App state={state} />, document.getElementById("root"));
