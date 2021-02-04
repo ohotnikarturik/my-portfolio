e02048030ed680526bea2c9c5346726d7621a18a
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2021-02-04 11:45:28 +0200

    add preload img

--- src/components/sections/HomeSection/HomeSection.js
+++ src/components/sections/HomeSection/HomeSection.js
@@ -1,20 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { NavLink } from "react-router-dom";
-import { Spring } from "react-spring/renderprops";
+import React from "react"
+import PropTypes from "prop-types"
+import { NavLink } from "react-router-dom"
+import { Spring } from "react-spring/renderprops"
+import PreloadImage from "react-preload-image"
 
-import backgroundImage from "../../../assets/img/bg/mountain1.jpg";
-import sprite from "../../../assets/img/sprite.svg";
-import style from "./HomeSection.module.scss";
-import SectionTitle from "../../SectionTitle";
-import SocialsItem from "../../SocialsItem";
-import ArrowDown from "../../ArrowDown";
-import Header from "../../Header";
+import backgroundImage from "../../../assets/img/bg/mountain1.jpg"
+import sprite from "../../../assets/img/sprite.svg"
+import style from "./HomeSection.module.scss"
+import SectionTitle from "../../SectionTitle"
+import SocialsItem from "../../SocialsItem"
+import ArrowDown from "../../ArrowDown"
+import Header from "../../Header"
 
 const HomeSection = ({ state }) => {
-  const bgImageStyle = (src) => ({
-    backgroundImage: `url(${src})`,
-  });
+  // const bgImageStyle = (src) => ({
+  //   backgroundImage: `url(${src})`,
+  // })
 
   return (
     <Spring
@@ -27,74 +28,71 @@ const HomeSection = ({ state }) => {
     >
       {(props) => (
         <div style={props}>
-          <section
-            id="Home"
-            style={bgImageStyle(backgroundImage)}
-            className={style.home}
-            title="Home"
-          >
-            <Header state={state} />
-            <div className={`container ${style.container}`}>
-              <div className={style.descContainer}>
-                <div className={style.profession}>
-                  <SectionTitle title={"Junior Full Stack Developer"} />
-                </div>
-                <div className={style.desc}>
-                  <h4 className={style.descText}>
-                    I can build web and mobile applications.
-                  </h4>
-                </div>
-                <div className={style.desc}>
-                  <div className={style.descText}>
-                    Code is my passion. I love{" "}
-                    <svg className={style.heartImg}>
-                      <use href={sprite + "#heart"} />
-                    </svg>{" "}
-                    to code every day. I like to create something new all the
-                    time. Meet new challenges and solve them. 
+          <PreloadImage src={backgroundImage} lazy className={style.home}>
+            <section id="Home" title="Home">
+              <Header state={state} />
+              <div className={`container ${style.container}`}>
+                <div className={style.descContainer}>
+                  <div className={style.profession}>
+                    <SectionTitle title={"Junior Full Stack Developer"} />
+                  </div>
+                  <div className={style.desc}>
+                    <h4 className={style.descText}>
+                      I can build web and mobile applications.
+                    </h4>
+                  </div>
+                  <div className={style.desc}>
+                    <div className={style.descText}>
+                      Code is my passion. I love{" "}
+                      <svg className={style.heartImg}>
+                        <use href={sprite + "#heart"} />
+                      </svg>{" "}
+                      to code every day. I like to create something new all the
+                      time. Meet new challenges and solve them. 
+                    </div>
+                  </div>
+                  <div className={style.linksContainer}>
+                    <div className={style.myCvContainer}>
+                      <NavLink
+                        className={style.link}
+                        to="/files/my-cv.pdf"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <div className={style.myCvTitle}>My CV</div>
+                        <svg className={style.openFileImg}>
+                          <use href={sprite + "#openPage"} />
+                        </svg>
+                      </NavLink>
+                    </div>
+                    <div className={style.socials}>
+                      <ul className={style.list}>
+                        {state.socials.map((item) => (
+                          <SocialsItem
+                            key={item.id}
+                            ariaLabel={item.ariaLabel}
+                            img={item.img}
+                            link={item.link}
+                          />
+                        ))}
+                      </ul>
+                    </div>
                   </div>
                 </div>
-                <div className={style.linksContainer}>
-                  <div className={style.myCvContainer}>
-                    <NavLink
-                      className={style.link}
-                      to="/files/my-cv.pdf"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      <div className={style.myCvTitle}>My CV</div>
-                      <svg className={style.openFileImg}>
-                        <use href={sprite + "#openPage"} />
-                      </svg>
-                    </NavLink>
-                  </div>
-                  <div className={style.socials}>
-                    <ul className={style.list}>
-                      {state.socials.map((item) => (
-                        <SocialsItem
-                          key={item.id}
-                          ariaLabel={item.ariaLabel}
-                          img={item.img}
-                          link={item.link}
-                        />
-                      ))}
-                    </ul>
-                  </div>
+                <div className={style.arrowDown}>
+                  <ArrowDown />
                 </div>
               </div>
-              <div className={style.arrowDown}>
-                <ArrowDown />
-              </div>
-            </div>
-          </section>
+            </section>
+          </PreloadImage>
         </div>
       )}
     </Spring>
-  );
-};
+  )
+}
 
 HomeSection.propTypes = {
   state: PropTypes.object.isRequired,
-};
+}
 
-export default HomeSection;
+export default HomeSection
